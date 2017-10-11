9b00491619d0f647c162639e0a135f022990baca
Barnard Baroi <barnardbaroi@Barnards-MacBook-Pro.local>
2017-10-11 15:54:22 +0600

    Added comments

--- mind_reader.py
+++ mind_reader.py
@@ -1,3 +1,5 @@
+'''This program will ask you to think of a number between 1 and 31 and will guess the 
+correct answer'''
 a=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31]
 b=[2,3,6,7,10,11,14,15,18,19,22,23,26,27,30,31]
 c=[8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31]
